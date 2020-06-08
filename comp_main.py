10b07847adcd1ccc743fecf7f92fc0366325a663
mikudehuane <1062352133@qq.com>
2020-06-08 16:45:52 +0800

    code reconstruct

--- comp_main.py
+++ comp_main.py
@@ -5,32 +5,67 @@ from csv_pic import *
 import csv
 import os.path as osp
 
-output_file = '.'.join([osp.split(sys.argv[0])[-1].split('.')[0], 'pdf'])
+"""
+:plot_order: define the order to plot lines 
+:legend_order: define the order to put legends, by defaut same with plot_order
 
+:legend_loc: location to put the legends
+:fraemon: whether to add fraemon to legends
+:label_spacing: label_spacing for legends
+
+:x_ticks: tuple of steps and text
+:x_annotation: annotation at the end of x_ticks, e.g. 10^3
+"""
+
+output_fp = '.'.join([osp.split(sys.argv[0])[-1].split('.')[0], 'pdf'])
 run_list = ['SGD', 'FedLaAvg-ICA', 'FedLaAvg-FCA', 'FedAvg-ICA', 'FedAvg-FCA', 'FedLaAvg-outlier']
 
+x_label = 'Communication Rounds'
+y_label = 'Test Accuracy (%)'
+
 legend_list = ['Sequential SGD', f'{alg} (ICA)', f'{alg} (FCA)',
-    'FedAvg (ICA)', 'FedAvg (FCA)', 'FedLaAvg (ICA with outliers)']
-
-# color_list = ['red', 'orange', 'blue', 'green', 'purple']
+    'FedAvg (ICA)', 'FedAvg (FCA)', 'FedLaAvg (ICA with Outliers)']
 color_list = ['red', 'orange', 'blue', 'green', 'purple', 'black']
-
 alpha_list = [1, 1, 1, 1, 1, 1]   # transparent degree
-
 line_list = ['--', '-', '-', ':', ':', '-']
-
 plot_every_list = [1, 1, 1, 1, 1, 100]
+line_width_list = [1, 1, 1, 1, 1, 1]
 
 max_it = 70000
 
-axin = [.7, .45, .2, .3]
 font = 21
 
-xlim = [69000, 70100]
-xticks = [69000, 70000]
-ylim = [53, 65]
-yticks = [54, 57, 60, 63]
+subfigure_pars = dict(
+    axin=[.7, .45, .2, .3],
+    xlim=[69000, 70100],
+    xticks=([69000, 70000], [69, 70]),
+    ylim=[53, 65],
+    yticks=[54, 57, 60, 63],
+    line_width=3.0,
+    x_annotation='$10^3$'
+)
 
-plot(run_list, legend_list, color_list, line_list, plot_every_list, output_file, max_it, font,
-    axin, xlim, ylim, xticks, yticks, filt=[0, 2, 5, 1, 4, 3], loc=8, alpha_list=alpha_list,
-    legend_order=[0, 2, 1, 4, 3, 5])
+plot_order = [0, 2, 5, 1, 4, 3]
+legend_order=[0, 2, 1, 4, 3, 5]
+
+legend_loc = "lower center"
+fraemon = False
+label_spacing = 0.4
+
+x_tick_interval = 10000
+x_ticks_step = list(range(0, max_it + 1, x_tick_interval))
+x_ticks_text = ['$' + str(num//1000) + '$' for num in x_ticks_step]
+x_ticks = x_ticks_step, x_ticks_text
+x_annotation = "$10^3$"
+
+plot(
+    run_list=run_list, output_fp=output_fp,
+    x_label=x_label, y_label=y_label,
+    legend_list=legend_list, color_list=color_list, line_list=line_list,
+    plot_every_list=plot_every_list, alpha_list=alpha_list, line_width_list=line_width_list,
+    max_it=max_it, font=font,
+    subfigure_pars=subfigure_pars,
+    plot_order=plot_order, legend_order=legend_order, 
+    legend_loc=legend_loc, fraemon=fraemon, label_spacing=label_spacing,
+    x_ticks=x_ticks, x_annotation=x_annotation
+)
--- csv_pic.py
+++ csv_pic.py
@@ -1,43 +1,87 @@
 import csv
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import os.path as osp
 import numpy as np
 from copy import deepcopy
+from matplotlib import rc
+from matplotlib import font_manager as fm, rcParams
 
 alg = 'FedLaAvg'
-def plot(run_list, legend_list, color_list, line_list, plot_every_list, output_file, max_it=1000000, font=15,
-    axin=None, xlim=None, ylim=None, xticks=None, yticks=None, reverse=False, filt=None, loc=None,
-    x_tick_interval=10000, alpha_list=None, legend_order=None):
+def plot(run_list, output_fp,
+    x_label=None, y_label=None,
+    legend_list=None, color_list=None, line_list=None, plot_every_list=None, alpha_list=None, line_width_list=None,
+    max_it=None, font=15,
+    subfigure_pars=None,
+    plot_order=None, legend_order=None, 
+    legend_loc=None, fraemon=True, label_spacing=0.4,
+    x_ticks=None, x_annotation=None):
+    """
+    :run_list: list of csv file names (discarding '.csv')
+    :output_fp: the output file name
 
-    num_lines = len(legend_list)
+    :x_label, y_label: labels
 
+    :legend_list: list of legends to be added to the figure
+    :color_list: list of line colors
+    :line_list: list of line styles
+    :plot_every_list: the minimum interval of points
+    :alpha_list: list of transparent degress
+    :line_width_list: list of line width
+
+    :max_it: maximum number of iterations
+    :font: font size for all text in the figure
+
+    :subfigure_pars: if not None, plot a subfigure with pars 
+        axin, xlim, yline, xticks (tuple), yticks (subfigure size, subfigure range, subfigure ticks)
+        line_width array or scalar (by default 3.0)
+        x_annotation
+    
+    :plot_order: define the order to plot lines 
+    :legend_order: define the order to put legends, by defaut same with plot_order
+
+    :legend_loc: location to put the legends
+    :fraemon: whether to add fraemon to legends
+    :label_spacing: label_spacing for legends
+
+    :x_ticks: tuple of steps and text
+    :x_annotation: annotation at the end of x_ticks, e.g. 10^3
+    """
+
+    # rc('text', usetex=True)
+    # rc('font',**{'family':'serif','serif':['Times New Roman'],'weight':'roman'})
+
+    # set math and ordinary fonts to be consistent
+    mpl.rcParams['mathtext.fontset'] = 'stix'
+    mpl.rcParams['font.family'] = 'STIXGeneral'
+    
+    num_lines = len(run_list)
+
+    if plot_every_list is None:
+        plot_every_list = [1] * num_lines
+    if legend_list is None:
+        legend_list = [None] * num_lines
+    if color_list is None:
+        color_list = [None] * num_lines
+    if line_list is None:
+        line_list = ['-'] * num_lines
     if alpha_list is None:
         alpha_list = [1] * num_lines
-    if filt is None:
-        filt = list(range(num_lines))
+    if line_width_list is None:
+        line_width_list = [1.0] * num_lines
+
+    if plot_order is None:
+        plot_order = list(range(num_lines))
     if legend_order is None:
-        legend_order = deepcopy(filt)
-
-    run_list = np.array(run_list)[filt].tolist()
-    legend_list = np.array(legend_list)[filt].tolist()
-    color_list = np.array(color_list)[filt].tolist()
-    line_list = np.array(line_list)[filt].tolist()
-    plot_every_list = np.array(plot_every_list)[filt].tolist()
-
-    filt_inverse = [-1] * len(filt)
-    for i, idx in enumerate(filt):
-        filt_inverse[idx] = i
-    
-    lines = [None] * len(legend_list)
-
-    if reverse:
-        run_list = run_list[::-1]
-        legend_list = legend_list[::-1]
-        color_list = color_list[::-1]
-        line_list = line_list[::-1]
-        plot_every_list = plot_every_list[::-1]
+        legend_order = deepcopy(plot_order)
 
     filep_list = [osp.join('data', run + '.csv') for run in run_list]
+
+    if x_ticks is None:
+        x_ticks = ()
+
+    lines = [None] * len(legend_list)
+
     steps_list = []
     values_list = []
 
@@ -54,7 +98,7 @@ def plot(run_list, legend_list, color_list, line_list, plot_every_list, output_f
         proc_steps = []
         proc_values = []
         for step, value in zip(steps, values):
-            if max_it >= step >= low:
+            if (max_it is None or max_it >= step) and step >= low:
                 proc_steps.append(step)
                 proc_values.append(value)
                 low += plot_every
@@ -65,40 +109,67 @@ def plot(run_list, legend_list, color_list, line_list, plot_every_list, output_f
 
     fig = plt.figure(dpi=128, figsize=(10, 6))
 
-    for line_idx, (steps, values, color, line, alpha, label) in enumerate(zip(steps_list, values_list, color_list, line_list, alpha_list, legend_list)):
-        lines[line_idx], = plt.plot(steps, values, line, c=color, linewidth=1.0, alpha=alpha, label=label)
+    plot_pars_array = list(zip(steps_list, values_list, color_list, line_list, alpha_list, legend_list, line_width_list))
+    for line_idx in plot_order:
+        steps, values, color, line, alpha, label, line_width  = plot_pars_array[line_idx]
+        pars_dict = {}
+        if color is not None:
+            pars_dict['c'] = color
+        if label is not None:
+            pars_dict['label'] = label
+        lines[line_idx], = plt.plot(steps, values, line, linewidth=line_width, alpha=alpha, **pars_dict)
 
-    font1 = {
-        'family' : 'Times New Roman',
+    font_dict = {
         'size'   : font,
     }
     
-    plt.xlabel('Communication Rounds', fontdict=font1)
-    plt.ylabel('Test Accuracy (%)', fontdict=font1)
-    ticks_x = [str(num//1000) for num in range(0, max_it + 50, x_tick_interval)]
-    plt.xticks(list(range(0, max_it + 50, x_tick_interval)), ticks_x, fontsize=font, fontfamily='Times New Roman')
-    plt.yticks(fontsize=font, fontfamily='Times New Roman')
+    if x_label is not None:
+        plt.xlabel(x_label, fontdict=font_dict)
+    if y_label is not None:
+        plt.ylabel(y_label, fontdict=font_dict)
 
-    handles = np.array(lines)[filt_inverse][legend_order].tolist()
-    plt.legend(handles=handles, prop=font1, frameon=False, labelspacing=0.4, loc=loc)
 
-    import matplotlib as mpl
+    plt.xticks(*x_ticks, fontsize=font)
+    plt.yticks(fontsize=font)
 
-    ticklabelpad = mpl.rcParams['xtick.major.pad']
-    plt.annotate('10$^\mathregular{3}$', xy=(1,0), ha='left', va='top',
-            xycoords='axes fraction', textcoords='offset points', fontfamily='Times New Roman', fontsize=font)
+    handles = [None] * len(legend_order)
+    for handles_idx, legend_idx in enumerate(legend_order):
+        handle = lines[legend_idx]
+        if handle is None:
+            print(f"legend_order {legend_order} contradicts plot_order {plot_order}, since require legends on lines that are not plotted.")
+            raise AssertionError()
+        handles[handles_idx] = handle
+    fig.legend(fancybox=True, handles=handles, prop=font_dict, frameon=fraemon, labelspacing=label_spacing, loc=legend_loc)
 
-    if axin is not None:
-        a = plt.axes(axin)
-        for steps, values, color, line in zip(steps_list, values_list, color_list, line_list):
-            plt.plot(steps, values, line, c=color, linewidth=3.0)
+    if x_annotation is not None:
+        plt.annotate(x_annotation, xy=(1,0), ha='left', va='top', xycoords='axes fraction', textcoords='offset points', fontsize=font)
+
+    if subfigure_pars is not None:
+        try:
+            axin = subfigure_pars['axin']
+            xlim, ylim = subfigure_pars['xlim'], subfigure_pars['ylim']
+            xticks, yticks = subfigure_pars['xticks'], subfigure_pars['yticks']
+        except KeyError as e:
+            print("When plotting the subfigure, not enough parameters provided")
+            raise e
+
+        subfigure_line_width = subfigure_pars.get('line_width', 3.0)
+        subfigure_x_annotation = subfigure_pars.get('x_annotation', None)
+
+        if isinstance(subfigure_line_width, float):
+            subfigure_line_width = [subfigure_line_width] * num_lines
+
+        plt.axes(axin)
+        for steps, values, color, line, width in zip(steps_list, values_list, color_list, line_list, subfigure_line_width):
+            plt.plot(steps, values, line, c=color, linewidth=width)
         plt.xlim(*xlim)
         plt.ylim(*ylim)
-        plt.xticks(xticks, [str(x // 1000) for x in xticks], size=font, family='Times New Roman')
-        plt.yticks(yticks, size=font, family='Times New Roman')
-        plt.annotate('10$^\mathregular{3}$', xy=(1,0), ha='left', va='top',
-                xycoords='axes fraction', textcoords='offset points', fontfamily='Times New Roman', fontsize=font)
+        plt.xticks(*xticks, size=font)
+        plt.yticks(yticks, size=font)
 
-    plt.tight_layout()
-    plt.savefig(osp.join('fig', output_file))
+        if subfigure_x_annotation is not None:
+            plt.annotate(subfigure_x_annotation, xy=(1,0), ha='left', va='top', xycoords='axes fraction', textcoords='offset points', fontsize=font)
+
+    fig.tight_layout()
+    fig.savefig(osp.join('fig', output_fp))
     plt.show()
